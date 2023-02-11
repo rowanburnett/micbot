e9c56b517f9bdc8fe6d8b5e8a411d9eb47904793
rowanburnett <rowan.sbur@gmail.com>
2023-02-11 23:50:27 +0000

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
